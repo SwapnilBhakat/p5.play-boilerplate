601542722ac5a54a9eb9fa4577ba6e413fba3d15
Swapnil Bhakat <66208539+SwapnilBhakat@users.noreply.github.com>
2020-06-02 12:22:01 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,32 @@
+var fixedrect,movingrect;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,800);
+ fixedrect =createSprite(400, 100, 50, 80);
+ fixedrect.shapeColor="green";
+ fixedrect.debug=true;
+ movingrect =createSprite(400, 800, 80, 30);
+ movingrect.shapeColor="green";
+ movingrect.debug=true;
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+  if(movingrect.x-fixedrect.x< fixedrect.width/2 + movingrect.width/2 &&
+fixedrect.x - movingrect.x < fixedrect.width/2 + movingrect.width/2  &&
+ movingrect.y - fixedrect.y < fixedrect.height/2 + movingrect.height/2
+  && fixedrect.y - movingrect.y < fixedrect.height/2 + movingrect.height/2
+  ){
+movingrect.shapeColor="red";
+fixedrect.shapeColor="red";
+  }
+
+else{
+  fixedrect.shapeColor="green";
+  movingrect.shapeColor="green";
+}
+  
   drawSprites();
 }
